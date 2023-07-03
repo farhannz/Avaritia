d092a2d6a5c202fac9016a4ef4ad66e9230c28e8
Farhan Nurzaman <57488803+farhannz@users.noreply.github.com>
2023-07-03 18:18:47 +0700

    update gitignore

--- src/gui/main_gui.cpp
+++ src/gui/main_gui.cpp
@@ -1,47 +1,7 @@
-// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
-// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
-// Read online: https://github.com/ocornut/imgui/tree/master/docs
 
 // #include <winbase.h>
-#include <processthreadsapi.h>
-#include <handleapi.h>
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-#include <imgui_spectrum.h>
-#include <ImGuiFileBrowser.h>
-#include <stdio.h>
-#include <cctype>
-#include <regex>
-#define GL_SILENCE_DEPRECATION
-#define IMGUI_USER_CONFIG "imgui_config.h"
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-#include <GLES2/gl2.h>
-#endif
-#include <GLFW/glfw3.h> // Will drag system OpenGL headers
-#include <iostream>
-
-
-
-
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
-// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
-// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
-#endif
-
-// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
-#ifdef __EMSCRIPTEN__
-#include "../libs/emscripten/emscripten_mainloop_stub.h"
-#endif
-
-static void glfw_error_callback(int error, const char* description)
-{
-    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
-}
-
+#include <gui/BaseApp.hpp>
+#include <GlobalSetting.hpp>
 
 void StartService(LPSTR lpApplicationName, bool &started, STARTUPINFO &si, PROCESS_INFORMATION &pi, std::vector<std::string> all_args){
     if(started) return;
@@ -78,110 +38,34 @@ void StartService(LPSTR lpApplicationName, bool &started, STARTUPINFO &si, PROCE
     CloseHandle( pi.hThread );
 }
 
+
+
 // Main code
 int main(int, char**)
 {
-    glfwSetErrorCallback(glfw_error_callback);
-    if (!glfwInit())
-        return 1;
+    avaritia::gui::BaseApp app("Avaritia Launcher", 600,300);
+    app.init();
 
-    // Decide GL+GLSL versions
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-    // GL ES 2.0 + GLSL 100
-    const char* glsl_version = "#version 100";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-#elif defined(__APPLE__)
-    // GL 3.2 + GLSL 150
-    const char* glsl_version = "#version 150";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
-#else
-    // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-#endif
-
-    // Create window with graphics context
-    GLFWwindow* window = glfwCreateWindow(600, 300, "Avaritia Launcher", nullptr, nullptr);
-    glfwSetWindowAttrib(window, GLFW_RESIZABLE, false);
-    if (window == nullptr)
-        return 1;
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
-
-    // App will launch at the center of the screen
-
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
-    //io.ConfigViewportsNoAutoMerge = true;
-    //io.ConfigViewportsNoTaskBarIcon = true;
-    {
-        int width, height;
-        const GLFWvidmode *vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-        glfwGetWindowSize(window, &width, &height);
-        glfwSetWindowPos(window, (vidmode->width - width)/2, (vidmode->height - height)/2);
-    }
-    
-    // Setup Dear ImGui style
-    // ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
-    ImGui::Spectrum::StyleColorsSpectrum(); 
-    ImGui::Spectrum::LoadFont(18.0f);
-    io.IniFilename = nullptr;
-    
-    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
-    ImGuiStyle& style = ImGui::GetStyle();
-    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-    {
-        style.WindowRounding = 0.0f;
-        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-    }
-    
-    // Setup Platform/Renderer backends
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init(glsl_version);
-
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != nullptr);
     // Our state
-    bool show_demo_window = false;
+    // bool show_demo_window = false;
     bool login_popup_window = false;
     bool setting_window = false;
     static std::string gamepath;
     static std::string _email, _pw;
     imgui_addons::ImGuiFileBrowser file_dialog;
+    bool settingWarning=false;
     bool service_started = false; // Service flag
     STARTUPINFO si;               // Service info
     PROCESS_INFORMATION pi;       // Service Process Info
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     DWORD exitCode;
+    avaritia::setting::GlobalSetting config;
+    bool isConfigExist = config.isConfigFileExist();
+    bool isAutoLogin = config.isAutoLogin();
+    if(!isConfigExist || (isConfigExist && config.getGuId().empty())) config.setGuId(avaritia::setting::DeviceID().ToString());
+    if(isConfigExist) {
+        config.loadConfig();
+    }
     // Main loop
 #ifdef __EMSCRIPTEN__
     // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
@@ -189,7 +73,7 @@ int main(int, char**)
     io.IniFilename = nullptr;
     EMSCRIPTEN_MAINLOOP_BEGIN
 #else
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(app.getWindow()))
 #endif
     {
         // Poll and handle events (inputs, window resize, etc.)
@@ -204,11 +88,6 @@ int main(int, char**)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
         ImGuiIO io = ImGui::GetIO();
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
             if(login_popup_window) ImGui::BeginDisabled();
             // static float f = 0.0f;
@@ -217,7 +96,7 @@ int main(int, char**)
             ImGui::SetNextWindowSize(ImGui::GetMainViewport()->Size,ImGuiCond_Always);
             ImGui::Begin("##begin",nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoDocking);                          // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            // ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
             
             static char email[64];
             static char password[32];
@@ -239,14 +118,12 @@ int main(int, char**)
             }
             ImGui::SameLine();
             if(ImGui::Button("Setting")) setting_window=true;
-            // ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            // ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-            // if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-            //     counter++;
-            // ImGui::SameLine();
-            // ImGui::Text("counter = %d", counter);
+            ImGui::SameLine();
+            ImGui::SameLine();
+            ImGui::SameLine();
+            ImGui::Checkbox("Save Account", &isAutoLogin); 
+            config.setAutoLogin(isAutoLogin);
 
-            // ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
             ImGui::End();
             ImGui::EndDisabled();
         }
@@ -262,9 +139,9 @@ int main(int, char**)
             ImGui::BeginPopupContextVoid("##login_notif");
             
 
-
             if(!service_started){
                 StartService(const_cast<char *>(std::string("main.exe").c_str()), service_started,si,pi,std::vector<std::string>{_email,_pw});
+                config.saveConfigToFile();
             }
             if (ImGui::Button("Close Me")){
                 login_popup_window = false;
@@ -272,11 +149,11 @@ int main(int, char**)
             ImGui::EndPopup();
         }
 
-        if (setting_window){
+        if (setting_window || !isConfigExist){
             bool browse = false;
             ImGui::SetNextWindowPos(ImGui::GetMainViewport()->Pos, ImGuiCond_Appearing, ImVec2(0,0));
             ImGui::SetNextWindowSize(ImGui::GetMainViewport()->Size,ImGuiCond_Always);
-            ImGui::Begin("Setting window",&setting_window, ImGuiWindowFlags_NoCollapse);
+            ImGui::Begin("Setting window",&setting_window, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoDocking);
             ImGui::Text("Game path :"); ImGui::SameLine();
             ImGui::InputText("##gamepath", const_cast<char*>(gamepath.c_str()),IM_ARRAYSIZE(gamepath.c_str()),ImGuiInputTextFlags_None,nullptr); ImGui::SameLine();
             if(ImGui::Button("Browse##gamepath")){
@@ -287,16 +164,30 @@ int main(int, char**)
             }
             file_dialog.showFileDialog("Browse", imgui_addons::ImGuiFileBrowser::DialogMode::SELECT,ImVec2(400,400));
             gamepath = file_dialog.selected_path.c_str();
+            
             if(ImGui::Button("Save")){
-                setting_window = false;
-                browse = false;
+                if(!gamepath.empty()){
+                    setting_window = false;
+                    browse = false;
+                    config.setGamePath(gamepath);
+                    config.setAutoLogin(isAutoLogin);
+                    config.saveConfigToFile();
+                    isConfigExist = true;
+                    settingWarning =false;
+                }
+                else{
+                    settingWarning = true;
+                }
+            }
+            if(settingWarning){
+                ImGui::Text("Please enter your game directory");
             }
             ImGui::End();
             // setting_window=false;
         }
         {
             GetExitCodeProcess(pi.hProcess, &exitCode);
-            ImGui::Text("%ld", exitCode);
+            // ImGui::Text("%ld", exitCode);
             if(service_started && exitCode == 0){
                 break;
             }
@@ -304,7 +195,7 @@ int main(int, char**)
         // Rendering
         ImGui::Render();
         int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glfwGetFramebufferSize(app.getWindow(), &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -321,19 +212,9 @@ int main(int, char**)
             glfwMakeContextCurrent(backup_current_context);
         }
         
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(app.getWindow());
     }
-#ifdef __EMSCRIPTEN__
-    EMSCRIPTEN_MAINLOOP_END;
-#endif
 
-    // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-
+    app.~BaseApp();
     return 0;
 }
