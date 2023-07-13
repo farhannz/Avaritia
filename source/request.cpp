122a9992d6a6fdd38e31a73d57387f5958b36a24
Farhan Nurzaman <57488803+farhannz@users.noreply.github.com>
2023-07-13 20:48:19 +0700

    move previous version as legacy

---
+++ source/request.cpp
@@ -0,0 +1,2 @@
+#include <avaritia/request.h>
+
---
+++ standalone/source/main.cpp
@@ -0,0 +1,11 @@
+
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+auto main(int argc, char** argv) -> int {
+
+  std::cout << "Hello world\n";
+  return 0;
+}
