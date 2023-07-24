aa6abdf743a9dd65fc831cd392e9b34cdb8a40d4
Farhan Nurzaman <57488803+farhannz@users.noreply.github.com>
2023-07-24 08:35:20 +0700

    Update baseapp.h

--- include/avaritia/baseapp.h
+++ include/avaritia/baseapp.h
@@ -1,7 +1,7 @@
 #pragma once
+#include <windows.h>
 #include <processthreadsapi.h>
 #include <handleapi.h>
-#include <windows.h>
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
